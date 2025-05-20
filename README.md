11fa3ac702c0e13b4a21f8b690b4d13a0aa67d56
Gláucia Andare <glauciaandare@gmail.com>
2025-05-20 19:11:28 -0300

    Configuração iniciais

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # projetos_csharp
+
+# Como criar um projeto novo
+
+```javascript
+dotnet new console -o ./CsharpProjects/TestProject
+```
+
+# Para realizar uma compilação do aplicativo, insira o seguinte comando no prompt de comando do Terminal:
+
+```javascript
+dotnet build
+```
---
+++ teste-basico/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Program
+{
+    static void Main()
+    {
+        int quantidade = int.Parse(Console.ReadLine());
+        List<int> despesas = new List<int>();
+
+        for (int i = 0; i < quantidade; i++)
+        {
+            int valor = int.Parse(Console.ReadLine());
+            // TODO: Adicione o valor à lista somente se for maior que zero
+            
+
+        }
+
+        if (despesas.Count == 0)
+        {
+            Console.WriteLine("Sem despesas validas");
+        }
+        else
+        {
+            // TODO: Imprima cada valor da lista de despesas válidas
+            
+
+        }
+    }
+}
